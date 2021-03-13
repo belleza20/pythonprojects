01ce90fb4d42c37147f991aa10cf887a80fb80ec
Enala Kwenda <47489747+nalakwenda@users.noreply.github.com>
2021-03-13 15:05:29 +0200

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import environ
-import dj_database_url
-import django_heroku
+
 
 
 
@@ -133,8 +132,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.settings(locals())
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+
 
