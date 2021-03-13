5589c34a702eeca343fff074c28e36be51bd9035
Enala Kwenda <47489747+nalakwenda@users.noreply.github.com>
2021-03-13 15:03:51 +0200

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'loveetcare',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
